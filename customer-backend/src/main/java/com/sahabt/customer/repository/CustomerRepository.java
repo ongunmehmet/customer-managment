e31f62c2d120f47af553a836b0be9f915a792862
Mehmet Emin Tiryaki <tiryakieminmehmet@gmail.com>
2022-05-10 22:38:15 +0300

    findCustomer is done

--- customer-backend/src/main/java/com/sahabt/customer/repository/CustomerRepository.java
+++ customer-backend/src/main/java/com/sahabt/customer/repository/CustomerRepository.java
@@ -4,7 +4,9 @@ import com.sahabt.customer.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,String> {
-
+    List<Customer> findCustomersByCompanyNameOrTaxNoOrCustomerIdOrSector(String companyName, String taxNo, String customerId, String sector);
 }
