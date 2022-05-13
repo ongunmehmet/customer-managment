c7770799c6f261a5bb949d9863e486610ae4e49d
RShbz <remzisahbaz@outlook.com>
2022-05-13 17:55:34 +0300

    backend - controller:
    add @PostMapping(value = "/add" ,produces = MediaType.APPLICATION_JSON_VALUE ,consumes = MediaType.APPLICATION_JSON_VALUE)

--- customer-backend/src/main/java/com/sahabt/customer/controller/CustomerRestController.java
+++ customer-backend/src/main/java/com/sahabt/customer/controller/CustomerRestController.java
@@ -5,21 +5,24 @@ import com.sahabt.customer.dto.request.CustomerUpdateRequest;
 import com.sahabt.customer.dto.request.GetInformantationCustomerRequest;
 import com.sahabt.customer.dto.response.CustomerAddResponse;
 import com.sahabt.customer.dto.response.CustomerResponse;
-import com.sahabt.customer.exception.CustomerNotFoundException;
+//import com.sahabt.customer.exception.CustomerNotFoundException;
 import com.sahabt.customer.model.Customer;
 import com.sahabt.customer.repository.CustomerRepository;
 import com.sahabt.customer.service.CustomerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.RequestScope;
 
+import java.lang.invoke.MethodType;
 import java.util.List;
 import java.util.Optional;
 
 @RequestScope
 @RestController
 @RequestMapping("/customers")
+@CrossOrigin
 public class CustomerRestController {
     private final CustomerService customerService;
     private final CustomerRepository customerRepository;
@@ -28,9 +31,17 @@ public class CustomerRestController {
         this.customerRepository = customerRepository;
     }
 
-    @PostMapping("/add")
+    @PostMapping(value = "/add" ,produces = MediaType.APPLICATION_JSON_VALUE ,consumes = MediaType.APPLICATION_JSON_VALUE)
     public Optional<CustomerAddResponse> createCustomer(@RequestBody CustomerAddRequest customerAddRequest) {
+
+
         return customerService.createCustomer(customerAddRequest);
+    }
+    @PostMapping(value = "/add2" ,produces = MediaType.APPLICATION_JSON_VALUE ,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerAddRequest createCustomer2(@RequestBody CustomerAddRequest customerAddRequest) {
+
+
+        return customerAddRequest;
     }
     @DeleteMapping("/delete/{id}")
     public Optional<CustomerResponse> deleteCustomerById(@PathVariable String id){
@@ -52,10 +63,10 @@ public class CustomerRestController {
     public List<CustomerResponse> findAllCustomers(@RequestParam(defaultValue = "0", required = false) int pageNo, @RequestParam(defaultValue = "20",required = false) int pageSize){
         return customerService.findAll(pageNo, pageSize);
     }
-
-    @ExceptionHandler(value = CustomerNotFoundException.class)
-    public ResponseEntity<Object> exception (CustomerNotFoundException exception){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
-    }
+//
+//    @ExceptionHandler(value = CustomerNotFoundException.class)
+//    public ResponseEntity<Object> exception (CustomerNotFoundException exception){
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+//    }
 
 }
