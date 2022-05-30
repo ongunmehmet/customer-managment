7c3fbe4dd97719d1655547051aeb09c1bd7c9930
Onur Sefa ORUÇOĞLU <onursefa.orucoglu@sahabt.com>
2022-05-30 11:17:04 +0300

    frontend changes with ultima-react old frontend keep in old-frontend folder

--- customer-frontend/src/components/container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Container(props){
-    return (
-        <div className="container" style={props.style}>
-            {props.children}
-        </div>
-    );
-}
